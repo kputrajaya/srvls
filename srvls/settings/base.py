1cfff469a48db58fabeb193754d4cf190b75651e
kiloev <freakperzon@yahoo.com>
2019-03-24 18:17:21 +0700

    Cache and session

--- srvls/settings/base.py
+++ srvls/settings/base.py
@@ -71,6 +71,19 @@ DATABASES = {
 }
 
 
+# Caching and session
+# https://docs.djangoproject.com/en/2.1/ref/settings/#cache
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'app-cache',
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cached_db'
+
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
