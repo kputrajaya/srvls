833e92cf7f68545b811833082fcbe74a79825289
kiloev <freakperzon@yahoo.com>
2019-03-27 22:43:48 +0700

    Hardcoded region

--- srvls/settings/base.py
+++ srvls/settings/base.py
@@ -129,7 +129,7 @@ STATIC_URL = '/static/'
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_REGION_NAME = os.environ.get('AWS_REGION', 'ap-southeast-1')
+AWS_S3_REGION_NAME = 'ap-southeast-1'
 AWS_STORAGE_BUCKET_NAME = os.environ.get('STORAGES_BUCKET_NAME')
 AWS_DEFAULT_ACL = None
 AWS_S3_CUSTOM_DOMAIN = os.environ.get('STORAGES_CUSTOM_DOMAIN')
